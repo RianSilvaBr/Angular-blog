e7d74fd11e51003a83771538ce687f113939ef41
rian <rian10401040@gmail.com>
2023-05-02 23:18:13 -0300

    "Content created"

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "title": " New Movie",
+    "description": "Hello",
+    "photoCover": "https://images.hdqwalls.com/wallpapers/i-am-iron-man-4k-ky.jpg"
+  },
+
+  {
+    "id": 2,
+    "title": " New Movie 2",
+    "description": "Hello 2",
+    "photoCover": ""
+  }
+
+]
